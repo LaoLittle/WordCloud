84d06ab8858e792afd15283f8a1dca397c3a3002
LaoLittle <y1941803875@outlook.com>
2022-01-19 18:46:15 +0800

    update

--- src/main/kotlin/MessageData.kt
+++
@@ -1,8 +0,0 @@
-package org.laolittle.plugin
-
-import org.jetbrains.exposed.sql.Table
-
-class MessageData(groupId: Long) : Table("messages_$groupId") {
-    val time = integer("time")
-    val content = text("content")
-}
